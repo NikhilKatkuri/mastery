c1382010b5672779cdabf19570257a81c5b6f76b
NikhilKatkuri <knikhil07k@gmail.com>
2025-09-18 10:59:14 +0530

    feat : added nunito font family, basic setup of metadata

---
+++ src/lib/baseMetaData.ts
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+const baseMetadata: Metadata = {
+  title: "Mastery",
+  description:
+    "A platform to master new skills and knowledge by playing games with solving questions.",
+  keywords: [
+    "Mastery",
+    "Skill Learning",
+    "Quiz Games",
+    "Gamified Learning",
+    "Education Platform",
+  ],
+  authors: [
+    { name: "NIKHIL KATKURI", url: "https://portfilo-livid.vercel.app/" },
+  ],
+  creator: "NIKHIL KATKURI",
+  publisher: "Mastery",
+  metadataBase: new URL("https://mastery.com"),
+  themeColor: "#090b0c",
+};
+
+export default baseMetadata;
